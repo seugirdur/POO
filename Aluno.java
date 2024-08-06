add8289b7f38ee48081b0d44b7fde25cc0c53606
Gabriel Rudrigues Lima <98267778+seugirdur@users.noreply.github.com>
2024-08-05 21:00:47 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,10 @@
+package com.aula;
+
+public class Aluno {
+    public String nome, cpf;
+    public int idade;
+
+    public void exibir() {
+        System.out.println(nome + " | " + cpf + " | " + idade);
+    }
+}
